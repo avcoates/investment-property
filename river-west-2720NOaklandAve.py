459ecb3e48ca671f90316daacf1a4c48b42b3490
avcoates <coatea62@uwosh.edu>
2021-03-22 09:07:50 -0500

    feat: river-west prop

---
+++ river-west-2720NOaklandAve.py
@@ -0,0 +1,18 @@
+from investment import analyzeDeal 
+
+
+# LETS DO SHOWING
+# Riverwest: 3 unit. good cashflow, need to see when leases expire
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20200921193319317241000000?_filter=StandardStatus+Eq+%27Active%27+And+PropertyClass+Eq+%27MultiFamily%27%2C%27Two-Family%27+And+MlsId+Eq+%2720080502212544227270000000%27%2C%2720010602000846355519000000%27+And+ListPrice+Bt+101000.0%2C350000.0+And+Location+Eq+polygon%28%2743.08620585973485+-87.91141425061035%2C43.05422844927357+-87.91016970562744%2C43.05510648324155+-87.88823996472168%2C43.07492161990803+-87.88137350964355%2C43.086832701010216+-87.87832652020263%27%29&listing_id=20210226213707309523000000
+# analyzeDeal(propertyPrice=338000,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=5531,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=3100,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3545,
+#     log=True)
+
+
---
+++ river-west-3052NBuffumSt.py
@@ -0,0 +1,57 @@
+from investment import analyzeDeal 
+
+# this property has insane potential.
+# if I can get rents up to 830 for 2 and 740 for 1, and rennovate, cashflow $13,000 after property management!!!
+# wtf
+# 3052 N Buffum St, Milwaukee, WI 53212 is a multi family home built in 1900. This property was last sold for $241,500 in 2017. The median sales price for the Harambee area is $61,250.
+
+# Sold for $241,500
+# Aug 26, 2016
+# Sold for $36,000
+
+# LETS DO SHOWING
+# Riverwest: cheap with rebate
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210112183324767463000000?_filter=StandardStatus+Eq+%27Active%27+And+PropertyClass+Eq+%27MultiFamily%27%2C%27Two-Family%27+And+MlsId+Eq+%2720080502212544227270000000%27%2C%2720010602000846355519000000%27+And+ListPrice+Bt+101000.0%2C350000.0+And+Location+Eq+polygon%28%2743.08620585973485+-87.91141425061035%2C43.05422844927357+-87.91016970562744%2C43.05510648324155+-87.88823996472168%2C43.07492161990803+-87.88137350964355%2C43.086832701010216+-87.87832652020263%27%29&listing_id=20210226213707309523000000
+
+# High est exp. low rent
+# analyzeDeal(propertyPrice=159000,
+#   downPayment=12000,
+#   interestRate=0.035,
+#   yearlyPropertyTaxes=2562,
+#   yearlyLandlordInsurance=2000,
+#   scheduledMonthlyIncome=1927,
+#   totalSquareFeet=1650,
+#   lotSize=0.1,
+#   additionalOperatingExpenses=5000,
+#   log=True)
+
+# High est exp. high rent
+# 2 bed: 830 x 3
+# 1 bed 740 x 1
+# 3220
+# analyzeDeal(propertyPrice=200000,
+#   downPayment=12000,
+#   interestRate=0.035,
+#   yearlyPropertyTaxes=2562,
+#   yearlyLandlordInsurance=2000,
+#   scheduledMonthlyIncome=2700,
+#   totalSquareFeet=1650,
+#   lotSize=0.1,
+#   additionalOperatingExpenses=5000,
+#   log=True)
+
+
+
+# High est exp. high rent, remodel 20,0000
+# 2 bed: 830 x 3
+# 1 bed 740 x 1
+# analyzeDeal(propertyPrice=159000,
+#   downPayment=12000,
+#   interestRate=0.035,
+#   yearlyPropertyTaxes=2562,
+#   yearlyLandlordInsurance=2000,
+#   scheduledMonthlyIncome=1927,
+#   totalSquareFeet=1650,
+#   lotSize=0.1,
+#   additionalOperatingExpenses=5000,
+#   log=True)
