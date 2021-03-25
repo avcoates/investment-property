5e1846a9dd033dff28663ac7e8cdeeb29015c9d5
avcoates <coatea62@uwosh.edu>
2021-03-24 19:18:02 -0500

    wip: update prop

---
+++ listings.py
@@ -0,0 +1,164 @@
+from investment import analyzeDeal 
+
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210305155907302365000000?_filter=ListPrice+Bt+101000.00%2C350000.00+And+StandardStatus+Eq+%27Active%27+And+MlsId+Eq+%2720010602000846355519000000%27%2C%2720080502212544227270000000%27+And+PropertyClass+Eq+%27MultiFamily%27%2C%27Two-Family%27+And+Location+Eq+polygon%28%2743.06404682344488+-87.90439811226311%2C43.08871710872226+-87.90422645088616%2C43.08965732968252+-87.87341323372307%2C43.073859702062116+-87.86826339241448%2C43.06194609896983+-87.87152495857659%2C43.050281065679556+-87.8898068952221%2C43.03434781245628+-87.89813247200432%2C43.03704546518481+-87.9069730329174%2C43.05128459663413+-87.9095479535717%2C43.05862241769789+-87.90388312813225%27%29
+# analyzeDeal(propertyPrice=275000,
+#   downPayment=12000,
+#   interestRate=0.035,
+#   yearlyPropertyTaxes=6373,
+#   yearlyLandlordInsurance=1400,
+#   scheduledMonthlyIncome=2275,
+#   totalSquareFeet=1650,
+#   lotSize=0.1,
+#   additionalOperatingExpenses=3000,
+#   log=True)
+
+
+# market
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210212223629246705000000
+# analyzeDeal(propertyPrice=199900,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=4827,
+#     yearlyLandlordInsurance=2000,
+#     scheduledMonthlyIncome=1800,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=300,
+#     log=True)
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# murray hill: current
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20201113015244957200000000
+# analyzeDeal(propertyPrice=269900,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=8342,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2400,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+# murrary hill: market
+# 2 x 1 1000
+# 4 x 1 1800
+# analyzeDeal(propertyPrice=269900,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=8342,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2800,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+# ------------------------------------------------------------------------------------------------------------------------
+
+# Murry hill: current
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210111215144498426000000
+# analyzeDeal(propertyPrice=249900,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=5940,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2265,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+# 
+# # Murry hill: market
+# 1 bed x 1 850
+# 5 bed x 1 2000
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210111215144498426000000
+# analyzeDeal(propertyPrice=249900,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=5940,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2850,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+
+# ------------------------------------------------------------------------------------------------------------------------
+# Murry hill: current
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210111213741378035000000
+# analyzeDeal(propertyPrice=284900,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=6613,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2520,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+
+# Murry hill: raise rent
+# 2 bed x 2 2000
+# 1 bed x 1 800
+# analyzeDeal(propertyPrice=284900,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=6613,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2800,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# bay view
+# analyzeDeal(propertyPrice=215000,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=3678,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=1850,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210322124912101637000000
+# bayview: marklet: 2 bed x 2 1100 x 2
+# analyzeDeal(propertyPrice=250000,
+#     downPayment=8000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=3678,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2200,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+
+# analyzeDeal(propertyPrice=250000,
+#     downPayment=8000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=3678,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=1200,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3000,
+#     log=True)
+
+
+
+# Murray Hill
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20201113015244957200000000
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210111215144498426000000
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210111213741378035000000
+
+# BayView
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210111213741378035000000
+
+# Riverwest
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210305155907302365000000
+# https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210212223629246705000000
--- river-west-2316NWeilSt.py
+++ river-west-2316NWeilSt.py
@@ -1,31 +1,70 @@
 from investment import analyzeDeal 
 
-
+# SOOOOOOOOOOOOOOOOOOLD
 # owner pays no utilities
 # LETS DO SHOWING it just works
 # Riverwest: 3 unit expensive but might work. not even adding a 2 bed yet
 # https://my.flexmls.com/chrisantonicci/search/saved_searches/20210317211216945809000000/listings/20210302193505263372000000?_filter=StandardStatus+Eq+%27Active%27+And+PropertyClass+Eq+%27MultiFamily%27%2C%27Two-Family%27+And+MlsId+Eq+%2720080502212544227270000000%27%2C%2720010602000846355519000000%27+And+ListPrice+Bt+101000.0%2C350000.0+And+Location+Eq+polygon%28%2743.08620585973485+-87.91141425061035%2C43.05422844927357+-87.91016970562744%2C43.05510648324155+-87.88823996472168%2C43.07492161990803+-87.88137350964355%2C43.086832701010216+-87.87832652020263%27%29&listing_id=20210226213707309523000000
-analyzeDeal(propertyPrice=329000,
-    downPayment=12000,
-    interestRate=0.035,
-    yearlyPropertyTaxes=7223,
-    yearlyLandlordInsurance=1500,
-    scheduledMonthlyIncome=2915,
-    totalSquareFeet=1650,
-    lotSize=0.1,
-    additionalOperatingExpenses=3545,
-    log=True)
+# analyzeDeal(propertyPrice=329000,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=7223,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2915,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=3545,
+#     log=True)
 
 # market rent and HIGH reserves
 # 3 bed 1200 x 2
 # 2 bed 1000 x 1
-analyzeDeal(propertyPrice=329000,
+# analyzeDeal(propertyPrice=329000,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=7223,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=3400,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=7200,
+#     log=True)
+
+# near tosa
+# https://my.flexmls.com/chrisantonicci/search/newsfeeds/listings/20210312225032791117000000
+# analyzeDeal(propertyPrice=156000,
+#     downPayment=7000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=3460,
+#     yearlyLandlordInsurance=1000,
+#     scheduledMonthlyIncome=1600,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=2400,
+#     log=True)
+
+# https://my.flexmls.com/chrisantonicci/search/newsfeeds/listings/20210305155907302365000000
+# analyzeDeal(propertyPrice=275000,
+#     downPayment=12000,
+#     interestRate=0.035,
+#     yearlyPropertyTaxes=6373,
+#     yearlyLandlordInsurance=1500,
+#     scheduledMonthlyIncome=2400,
+#     # scheduledMonthlyIncome=2275,
+#     totalSquareFeet=1650,
+#     lotSize=0.1,
+#     additionalOperatingExpenses=4000,
+#     log=True)
+
+
+analyzeDeal(propertyPrice=215000,
     downPayment=12000,
     interestRate=0.035,
-    yearlyPropertyTaxes=7223,
+    yearlyPropertyTaxes=3678,
     yearlyLandlordInsurance=1500,
-    scheduledMonthlyIncome=3400,
+    # scheduledMonthlyIncome=2000,
+    scheduledMonthlyIncome=1800,
     totalSquareFeet=1650,
     lotSize=0.1,
-    additionalOperatingExpenses=7200,
+    additionalOperatingExpenses=4000,
     log=True)
